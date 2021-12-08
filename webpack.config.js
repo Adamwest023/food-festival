442284ba54afb36367db130594e067978099ec86
= <=>
2021-12-08 08:28:09 -0600

    code split

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 //call for requirements in path and webpack
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 const path = require("path");
 const webpack  = require("webpack");
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app:'./assets/js/script.js',
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.bundle.js'
@@ -14,6 +20,9 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: "static", //the report outputs to an HTML file in the dist folder
+        })
     ],
     mode: 'development'
 }
